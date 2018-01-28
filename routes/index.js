5b91aec5072b3e20ffacaf4c233274d29bcb1212
Greg Malysa <gmalysa@stanford.edu>
2018-01-27 18:57:11 -0600

    Added rules route and nav link

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 /**
- * Route that handles the index/landing page
+ * Route that handles the simple and static content pages
  */
+
+var fs = require('fs');
 
 /**
  * Just show the main page
@@ -11,9 +13,34 @@ function index(env, after) {
 	after();
 }
 
+/**
+ * Show the generated rules content
+ */
+function rules(env, after) {
+	env.$template('rules');
+	fs.readFile('static/markdown/rules.md', function(err, data) {
+		if (err) {
+			env.$throw(new Error('Unable to read rules file.'));
+			return;
+		}
+
+		env.$output({
+			title: 'Rules - Learn Dota 2 League',
+			rules: data
+		});
+		after();
+	});
+}
+
 module.exports.init_routes = function(server) {
 	server.add_route('/', {
 		fn : index,
+		pre : ['default'],
+		post : ['default']
+	}, 'get');
+
+	server.add_route('/rules', {
+		fn : rules,
 		pre : ['default'],
 		post : ['default']
 	}, 'get');
