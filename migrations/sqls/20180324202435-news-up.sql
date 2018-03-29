4943f848ceec1e7577ff18d4beb7bc67a6d969f2
Greg Malysa <gmalysa@stanford.edu>
2018-03-29 01:51:31 +0000

    Handle news time via trigger for compatibility with older mysql install

--- migrations/sqls/20180324202435-news-up.sql
+++ migrations/sqls/20180324202435-news-up.sql
@@ -1,9 +1,12 @@
 CREATE TABLE `news` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `posted` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `posted` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
   `author` varchar(25) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TRIGGER news_date BEFORE INSERT on `news`
+FOR EACH ROW SET NEW.posted = NOW(), NEW.updated = NOW();
