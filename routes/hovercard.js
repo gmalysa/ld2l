2a12c8bf677f8e0f271fb9c165cbaf6bead7a75b
Greg Malysa <gmalysa@stanford.edu>
2019-03-30 17:23:00 -0500

    Initial profile hovercard implementation

--- routes/hovercard.js
+++ routes/hovercard.js
@@ -5,6 +5,7 @@
 var _ = require('underscore');
 var fl = require('flux-link');
 var db = require('db-filters');
+var dotaconstants = require('dotaconstants');
 
 var users = require('../lib/users.js');
 var teams = require('../lib/teams.js');
@@ -39,6 +40,44 @@ function hoverTeamInfo(team) {
 }
 
 /**
+ * Convert hero ID and extra info into an object that the client can actually use,
+ * without needing dotaconstants
+ */
+function hoverHeroDetails(hero) {
+	var dchero = dotaconstants.heroes[hero.hero];
+
+	if (dchero) {
+		return {
+			name : dchero.localized_name,
+			img : 'http://cdn.dota2.com/'+dchero.img,
+			wins : hero.wins,
+			losses : hero.losses
+		}
+	}
+	else {
+		return {
+			name : 'Unknown hero',
+			img : '',
+			wins : hero.wins,
+			losses : hero.losses
+		};
+	}
+}
+
+/**
+ * Create summary object for filling in the hovercard
+ */
+function hoverProfileInfo(user, signup, played) {
+	return {
+		steamid : user.steamid,
+		avatar : user.avatar,
+		display_name : user.display_name,
+		medal : signup.medal,
+		played : played.slice(0, 5).map(hoverHeroDetails),
+	}
+}
+
+/**
  * Generate the hovercard information for a team
  */
 var hovercard_team = new fl.Chain(
@@ -66,6 +105,32 @@ var hovercard_team = new fl.Chain(
  * Generate hovercard information for a player
  */
 var hovercard_player = new fl.Chain(
+	function(env, after) {
+		after(env.req.body.id);
+	},
+	users.getUser,
+	function(env, after, user) {
+		if (user === null) {
+			env.$throw(new Error('Unable to find a matching user'));
+			return;
+		}
+
+		env.hoverUser = user;
+		after(user);
+	},
+	users.getRecentSignup,
+	function(env, after, signup) {
+		env.signup = signup;
+		after(env.hoverUser);
+	},
+	matches.getMostPlayed,
+	function(env, after, played) {
+		env.$json({
+			hoverTemplate : 'hover_profile',
+			hoverData : hoverProfileInfo(env.hoverUser, env.signup, played)
+		});
+		after();
+	}
 ).use_local_env(true);
 
 /**
